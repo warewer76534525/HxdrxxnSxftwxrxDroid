a8f3f125dea0f289d93e4b5f2554890492b2df33
Jaka Tambunan <jakaputralesmana@yahoo.com>
2011-10-03 10:15:51 +0700

    initial commit

---
+++ HidreenSoftwareAndroid/src/com/triplelands/HidreenSoftware/MainActivity.java
@@ -0,0 +1,12 @@
+package com.triplelands.HidreenSoftware;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
